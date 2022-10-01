6ed23d808735be60b4e4ee7aaf0592a00a37607d
SanketMungra <68103539+SanketMungra@users.noreply.github.com>
2022-10-01 10:17:00 +0530

    Update qe.py (#19)

--- qe.py
+++ qe.py
@@ -13,5 +13,7 @@ d = (b**2) - (4*a*c)
 # find two solutions
 sol1 = (-b-cmath.sqrt(d))/(2*a)
 sol2 = (-b+cmath.sqrt(d))/(2*a)
+sol3 = (-a+cmath.sqrt(c))/(2*b)
 
-print('The solution are {0} and {1}'.format(sol1,sol2))
+
+print('The solution are {0} and {1}'.format(sol1,sol2,sol3))
